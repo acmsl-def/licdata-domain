aa8b5e5c3565c6a268025870b1db6eaac06f9bf1
Jose San Leandro Armendáriz <github@acm-sl.org>
2025-01-30 11:42:04 +0100

    Commit created with sync-pythoneda.sh

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     acmsl-licdata-events = {
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
-      url = "github:acmsl-def/licdata-events/0.0.29";
+      url = "github:acmsl-def/licdata-events/0.0.30";
       inputs.pythoneda-shared-pythonlang-banner.follows =
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
@@ -40,7 +40,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.pythoneda-shared-pythonlang-banner.follows =
         "pythoneda-shared-pythonlang-banner";
-      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.124";
+      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.126";
     };
   };
   outputs = inputs:
